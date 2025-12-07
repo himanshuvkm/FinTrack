1cbf2c8b58e9eaf98210cbae85f69b51e74253a5
himanshuvkm <himanshuvkm252@gmail.com>
2025-12-08 01:23:03 +0530

    login issue  fixed

--- components/compo/header.tsx
+++ components/compo/header.tsx
@@ -9,8 +9,10 @@ import {
 import { Wallet, LayoutDashboard, PenBox } from 'lucide-react';
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { checkUser } from "@/lib/checkUser";
 
-export default function Header() {
+export default async function Header() {
+  await checkUser();
   return (
     <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
